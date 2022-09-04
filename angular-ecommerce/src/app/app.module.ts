3287287726998df47a32b5481b6346e91ae216a5
Yıldıray Abdioğlu <yabdioglu37@gmail.com>
2022-09-05 02:39:41 +0300

    08-search-for-products-by-category

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { Product } from './common/product';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -6,12 +7,30 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
 
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  /*
+  Order of routes is important
+  First match winds.
+  Start from most specific to generic
+  */
+  { path: 'category/:id', component: ProductListComponent }, // when path matches, create new instance of component
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '""', redirectTo: '/products', pathMatch: 'full' }, // This is the generic wildcard.
+  // It will match on anything that didn't match above routes.
+
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],

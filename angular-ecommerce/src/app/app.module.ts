9371b34242d4d4d78d99aca266c15148016278f1
Yıldıray Abdioğlu <yabdioglu37@gmail.com>
2022-09-04 11:35:30 +0300

    06-angular-product-list

--- angular-ecommerce/src/app/app.module.ts
+++ angular-ecommerce/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductService } from './services/product.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ProductService],
+  /*
+  "providers": this will allow us to inject 
+  that given service into other parts of our application.
+  */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
